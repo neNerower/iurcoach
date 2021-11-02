7a754ab8c0ed5dda527758bdab76c4c9a82646c8
Nerower <p0sirega121234@gmail.com>
2021-11-02 19:54:09 +0300

    theme: button text style updated

--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 
 ThemeData mainTheme() => ThemeData(
-  brightness: Brightness.light,
-  primarySwatch: Colors.blue,
-  fontFamily: 'ROBOTO',
-);
+      brightness: Brightness.light,
+      primarySwatch: Colors.blue,
+      textTheme: TextTheme(
+        button: TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.w600,
+        ),
+      ),
+      fontFamily: 'ROBOTO',
+    );
