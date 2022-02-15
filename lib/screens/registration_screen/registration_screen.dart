734c45dfa733ed23634f90b6d02ceb6be001b405
Nerower <p0sirega121234@gmail.com>
2022-02-16 00:29:08 +0300

    reg_screen: Turn changes back

--- lib/screens/registration_screen/registration_screen.dart
+++ lib/screens/registration_screen/registration_screen.dart
@@ -1,109 +1,81 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:iurc_mobile_app/components/base_button.dart';
+
 import 'package:iurc_mobile_app/components/form_fields/base_form_field.dart';
 import 'package:iurc_mobile_app/components/form_fields/email_field.dart';
 import 'package:iurc_mobile_app/components/form_fields/isu_field.dart';
+import 'package:iurc_mobile_app/components/form_fields/password_field.dart';
 import 'package:iurc_mobile_app/components/form_fields/phone_field.dart';
 import 'package:iurc_mobile_app/components/form_fields/vk_id_field.dart';
-import 'package:iurc_mobile_app/components/readonly_tab_bar.dart';
 
 class RegistrationScreen extends StatefulWidget {
-  const RegistrationScreen({Key? key}) : super(key: key);
+  RegistrationScreen({Key? key}) : super(key: key);
 
   @override
-  _RegistrationScreenState createState() => _RegistrationScreenState();
+  State<RegistrationScreen> createState() => _RegistrationScreenState();
 }
 
-class _RegistrationScreenState extends State<RegistrationScreen>
-    with SingleTickerProviderStateMixin {
+class _RegistrationScreenState extends State<RegistrationScreen> {
   String _firstName = "";
   String _lastName = "";
-  DateTime _birthdate = DateTime(0);
   String _isuNumber = "";
   String _phoneNumber = "";
   String _vkIdNumber = "";
   String _email = "";
 
-  late TabController _tabController;
-  int _currentIndex = 0;
+  void _sendRegistration(BuildContext context) {
+    if (!_formKey.currentState!.validate()) {
+      return;
+    }
+
+    // TODO: send registration request
+    Navigator.pushNamedAndRemoveUntil(context, "/home", (route) => false);
+  }
+
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-
-  // Tabs names
-  static const List<Tab> _tabs = <Tab>[
-    Tab(text: "1"),
-    Tab(text: "2"),
-    Tab(text: "3")
-  ];
-
-  @override
-  void initState() {
-    super.initState();
-    _tabController = TabController(length: _tabs.length, vsync: this);
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    _tabController.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Scaffold(
-        resizeToAvoidBottomInset: true,
-        appBar: AppBar(
-          title: Text("Регистрация"),
-          bottom: ReadonlyTabBar(
-            child: TabBar(
-              tabs: _tabs,
-              controller: _tabController,
-            ),
-          ),
-        ),
-        body: Padding(
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Регистрация"),
+      ),
+      resizeToAvoidBottomInset: true,
+      body: SingleChildScrollView(
+        child: Padding(
+          // TODO: extract to user_data_form
           padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
           child: Form(
             key: _formKey,
-            child: TabBarView(
-              physics: NeverScrollableScrollPhysics(),
-              controller: _tabController,
+            child: Column(
               children: [
-                Column(
-                  children: [
-                    BaseFormField(
-                      labelText: "Имя",
-                      onSaved: (value) => _firstName = value!,
-                    ),
-                    BaseFormField(
-                      labelText: "Фамилия",
-                      onSaved: (value) => _lastName = value!,
-                    ),
-                    CupertinoDatePicker(
-                      onDateTimeChanged: (DateTime value) => ,
-                      mode: CupertinoDatePickerMode.date,
-
-                    ),
-                    BaseButton(
-                      label: "Далее",
-                      onPressed: () =>
-                          _tabController.animateTo(_currentIndex += 1),
-                    )
-                  ],
+                BaseFormField(
+                  labelText: "Имя",
+                  onSaved: (value) => _firstName = value!,
                 ),
-                Column(
-                  children: [
-                    PhoneField(
-                      onSaved: (value) => _phoneNumber = value!,
-                    ),
-                    VkIdField(
-                      onSaved: (value) => _vkIdNumber = value!,
-                    ),
-                    EmailField(
-                      onSaved: (value) => _email = value!,
-                    ),
-                  ],
+                BaseFormField(
+                  labelText: "Фамилия",
+                  onSaved: (value) => _lastName = value!,
+                ),
+                IsuField(
+                  onSaved: (value) => _isuNumber = value!,
+                ),
+                PhoneField(
+                  onSaved: (value) => _phoneNumber = value!,
+                ),
+                VkIdField(
+                  onSaved: (value) => _vkIdNumber = value!,
+                ),
+                EmailField(
+                  onSaved: (value) => _email = value!,
+                ),
+                PasswordField(
+                  labelText: "Пароль",
+                  onSaved: (value) => {},
+                ),
+                BaseButton(
+                  label: "Отправить",
+                  onPressed: () => _sendRegistration(context),
                 ),
               ],
             ),
