c64cce90130de279c810a73d9f34e7bd88c418d4
Nerower <p0sirega121234@gmail.com>
2022-03-03 00:04:09 +0300

    rebuild: Add base screens

---
+++ lib/screens/signin_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SigninScreen extends StatefulWidget {
+  SigninScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SigninScreen> createState() => _SigninScreenState();
+}
+
+class _SigninScreenState extends State<SigninScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
