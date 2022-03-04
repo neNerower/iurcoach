f902b2388c96a02cfa51f5fcac4ee28fc1fc12f5
Nerower <p0sirega121234@gmail.com>
2022-03-04 23:47:24 +0300

    rebuild: Add fields to signup_screen

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -8,8 +8,76 @@ class SignupScreen extends StatefulWidget {
 }
 
 class _SignupScreenState extends State<SignupScreen> {
+  String _firstName = "";
+  String _lastName = "";
+  String _isuID = "";
+  String _vkId = "";
+  String _phoneNumber = "";
+  String _email = "";
+  DateTime? _birthdate;
+
+  final _formKey = GlobalKey<FormState>();
+
+  void _sendRegistration(BuildContext context) {
+    // TODO: send registration request
+    Navigator.pushNamedAndRemoveUntil(context, "/home", (route) => false);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      resizeToAvoidBottomInset: true,
+      appBar: AppBar(
+        title: const Text("Регистрация"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+        child: SingleChildScrollView(
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: "Имя",
+                  ),
+                  onSaved: (val) => _firstName = val ?? "",
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: "Фамилия",
+                  ),
+                  onSaved: (val) => _lastName = val ?? "",
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: "Номер ИСУ",
+                  ),
+                  onSaved: (val) => _isuID = val ?? "",
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: "ID в «ВКонтакте»",
+                  ),
+                  onSaved: (val) => _vkId = val ?? "",
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: "Номер телефона",
+                  ),
+                  onSaved: (val) => _phoneNumber = val ?? "",
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: "Email",
+                  ),
+                  onSaved: (val) => _email = val ?? "",
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
