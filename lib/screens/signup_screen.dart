8b0f9c0be22f2cd3edb0bf56443e8eaa263766a3
Nerower <p0sirega121234@gmail.com>
2022-03-05 02:06:36 +0300

    signup: Complete signup screen fields

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:iurc_mobile_app/widgets/date_form_field.dart';
 
 class SignupScreen extends StatefulWidget {
   SignupScreen({Key? key}) : super(key: key);
@@ -14,11 +16,12 @@ class _SignupScreenState extends State<SignupScreen> {
   String _vkId = "";
   String _phoneNumber = "";
   String _email = "";
+  String _password = "";
   DateTime? _birthdate;
 
   final _formKey = GlobalKey<FormState>();
 
-  void _sendRegistration(BuildContext context) {
+  void _signup(BuildContext context) {
     // TODO: send registration request
     Navigator.pushNamedAndRemoveUntil(context, "/home", (route) => false);
   }
@@ -30,9 +33,9 @@ class _SignupScreenState extends State<SignupScreen> {
       appBar: AppBar(
         title: const Text("Регистрация"),
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
-        child: SingleChildScrollView(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
           child: Form(
             key: _formKey,
             child: Column(
@@ -49,6 +52,9 @@ class _SignupScreenState extends State<SignupScreen> {
                   ),
                   onSaved: (val) => _lastName = val ?? "",
                 ),
+                // DateFormField(
+                //   onSaved: (val) => _birthdate = val,
+                // ),
                 TextFormField(
                   decoration: const InputDecoration(
                     labelText: "Номер ИСУ",
@@ -72,6 +78,29 @@ class _SignupScreenState extends State<SignupScreen> {
                     labelText: "Email",
                   ),
                   onSaved: (val) => _email = val ?? "",
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: "Пароль"
+                  ),
+                  onSaved: (val) => _password = val ?? "",
+                  obscureText: true,
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: "Подтвердить пароль"
+                  ),
+                  obscureText: true,
+                ),
+
+                Container(
+                  margin: const EdgeInsets.only(top: 5, bottom: 20),
+                  height: 45,
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    child: Text("Сохранить"),
+                    onPressed: () => _signup(context),
+                  ),
                 ),
               ],
             ),
