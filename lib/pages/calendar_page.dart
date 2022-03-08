75de099c0559d262c9a77f61494e59025bc33d11
Nerower <p0sirega121234@gmail.com>
2022-03-08 23:58:18 +0300

    calendar_screen: Add calendar widget

--- lib/pages/calendar_page.dart
+++ lib/pages/calendar_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class CalendarPage extends StatefulWidget {
   CalendarPage({Key? key}) : super(key: key);
@@ -12,9 +13,17 @@ class CalendarPage extends StatefulWidget {
 class _CalendarPageState extends State<CalendarPage> {
   @override
   Widget build(BuildContext context) {
+    var _currentDate = DateTime.now();
+    var _focusedDate = _currentDate;
+
+
     return Container(
       child: Center(
-        child: Text(widget.title),
+        child: TableCalendar(
+          firstDay: DateTime(2016, 1, 1),
+          lastDay: DateTime(_currentDate.year + 1, 1, 1),
+          focusedDay: _focusedDate,
+        ),
       ),
     );
   }
