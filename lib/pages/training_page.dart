75676921c25b523e931277cbc51bd5aa3d32015f
Nerower <p0sirega121234@gmail.com>
2022-03-10 00:07:40 +0300

    training_page: Add training_preview list builder

--- lib/pages/training_page.dart
+++ lib/pages/training_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:iurc_mobile_app/models/training_model.dart';
+import 'package:iurc_mobile_app/widgets/training_preview.dart';
 
 class TrainingPage extends StatefulWidget {
   TrainingPage({Key? key}) : super(key: key);
@@ -10,11 +12,37 @@ class TrainingPage extends StatefulWidget {
 }
 
 class _TrainingPageState extends State<TrainingPage> {
+  late List<TrainingModel> _trainings;
+
+  @override
+  void initState() {
+    super.initState();
+    // TODO: create lasy load list veiw instead of this
+    _trainings = [
+      TrainingModel(
+        dateTime: DateTime(2022, 3, 10, 18, 00),
+        place: "Strela",
+        plan: ["Run 30`", "RP3 20x40", "Gym"],
+      ),
+      TrainingModel(
+        dateTime: DateTime(2022, 3, 11, 16, 30),
+        place: "Lomo",
+        plan: ["Concept 6km", "2x30`", "OFP"],
+      ),
+    ];
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text(widget.title),
+    return Center(
+      child: ListView.builder(
+        itemCount: _trainings.length,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: TrainingPreview(model: _trainings[index]),
+          );
+        },
       ),
     );
   }
