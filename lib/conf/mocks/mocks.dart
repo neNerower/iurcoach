e59473207395e329a604a40ae9674acc7931d117
Nerower <p0sirega121234@gmail.com>
2022-03-11 00:08:27 +0300

    conf: Complete mocks with appUser stub

--- lib/conf/mocks/mocks.dart
+++ lib/conf/mocks/mocks.dart
@@ -1,7 +1,7 @@
 import 'package:iurc_mobile_app/models/models.dart';
 
-class EventMocks {
-  EventMocks._();
+class Mocks {
+  Mocks._();
 
   static List<TrainingModel> events = [
     TrainingModel(
@@ -14,7 +14,6 @@ class EventMocks {
       place: "Lomo",
       plan: ["Concept 6km", "2x30`", "OFP"],
     ),
-
     TrainingModel(
       dateTime: DateTime(2022, 3, 9, 18, 00),
       place: "Strela",
@@ -25,7 +24,6 @@ class EventMocks {
       place: "Strela",
       plan: ["Concept 6km", "2x30`", "OFP"],
     ),
-
     TrainingModel(
       dateTime: DateTime(2022, 3, 11, 18, 00),
       place: "Lomo",
@@ -36,7 +34,6 @@ class EventMocks {
       place: "Lomo",
       plan: ["Concept 6km", "2x30`", "OFP"],
     ),
-    
     TrainingModel(
       dateTime: DateTime(2022, 3, 16, 18, 00),
       place: "Lomo",
@@ -48,4 +45,16 @@ class EventMocks {
       plan: ["Concept 6km", "2x30`", "OFP"],
     ),
   ];
+
+  static UserModel appUser = UserModel(
+    firstName: "Ne",
+    lastName: "Nerower",
+    birthdate: DateTime(2000, 1, 1),
+    group: "Мужская сборная",
+    isuID: "123456",
+    vkID: "vk.com/nerower",
+    phoneNumber: "+79991110022",
+    email: "nerower@mail.com",
+    photoURL: "https://cdn.vox-cdn.com/thumbor/KmUSGKjWbKxOCahy4yZkw17HZ64=/0x0:2370x1574/920x613/filters:focal(996x598:1374x976):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/68870438/Screen_Shot_2020_07_21_at_9.38.25_AM.0.png"
+  );
 }
