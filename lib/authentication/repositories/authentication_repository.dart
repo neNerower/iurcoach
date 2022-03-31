101af855d4118518abde6a50c7ff977ae37114dd
Nerower <p0sirega121234@gmail.com>
2022-03-31 18:01:56 +0300

    auth: Simplify repositories

--- lib/authentication/repositories/authentication_repository.dart
+++ lib/authentication/repositories/authentication_repository.dart
@@ -3,10 +3,7 @@ import 'dart:async';
 import 'package:dio/dio.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
-enum AuthenticationStatus { unknown, authenticated, unauthenticated }
-
 class AuthenticationRepository {
-  final _controller = StreamController<AuthenticationStatus>();
   final _storage = FlutterSecureStorage();
 
   final _dio = Dio(BaseOptions(
@@ -15,11 +12,6 @@ class AuthenticationRepository {
     receiveTimeout: 3000,
   ));
 
-  Stream<AuthenticationStatus> get status async* {
-    await Future<void>.delayed(const Duration(seconds: 1));
-    yield AuthenticationStatus.unauthenticated;
-    yield* _controller.stream;
-  }
 
   Future<void> logIn({
     required String username,
@@ -30,19 +22,21 @@ class AuthenticationRepository {
       // Save jwt to secure storage
       await _storage.write(key: 'access_token', value: response.data['access_token']);
       await _storage.write(key: 'refresh_token', value: response.data['refresh_token']);
-
-      () => _controller.add(AuthenticationStatus.authenticated);
+    } else {
+      throw AuthenticationException("Incorrect credentials !");
     }
-    // TODO: Throw exception if not succeeded
   }
 
   Future<void> logOut() async {
-    // Removing jwt from secure storage
+    // Remove jwt from secure storage
     await _storage.delete(key: 'access_token');
     await _storage.delete(key: 'refresh_token');
-
-    _controller.add(AuthenticationStatus.unauthenticated);
   }
+}
 
-  void dispose() => _controller.close();
+
+class AuthenticationException implements Exception {
+  final String message;
+
+  AuthenticationException(this.message);
 }
--- lib/authentication/repositories/credentials_repository.dart
+++ lib/authentication/repositories/credentials_repository.dart
@@ -3,16 +3,15 @@ import '../models/credentials.dart';
 
 class CredentialsRepository {
   final _storage = FlutterSecureStorage();
-  Credentials? _credentials;
 
   Future<Credentials?> getCredentials() async {
-    if (_credentials != null)
-      return _credentials;
-
     final accessToken = await _storage.read(key: 'access_token');
     final refreshToken = await _storage.read(key: 'refresh_token');
-    _credentials = Credentials(accessToken ?? "", refreshToken ?? "");
 
-    return _credentials;
+    if (accessToken != null && refreshToken != null) {
+      return Credentials(accessToken, refreshToken);
+    }
+
+    return null;
   }
 }
