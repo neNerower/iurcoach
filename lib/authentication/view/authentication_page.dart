9b0db2abc9d7b4651dbb813f3fc4469301848771
Nerower <p0sirega121234@gmail.com>
2022-04-04 02:38:53 +0300

    auth: Fix screens navigations

--- lib/authentication/view/authentication_page.dart
+++ lib/authentication/view/authentication_page.dart
@@ -5,19 +5,19 @@ import '../authentication.dart';
 import 'authentication_view.dart';
 
 class AuthenticationPage extends StatelessWidget {
-  final authenticationRepository = AuthenticationRepository();
-  final tokensRepository = TokensRepository();
+  final _authenticationRepository = AuthenticationRepository();
+  final _tokensRepository = TokensRepository();
 
   AuthenticationPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return RepositoryProvider.value(
-      value: authenticationRepository,
+      value: _authenticationRepository,
       child: BlocProvider(
         create: (_) => AuthenticationBloc(
-          authenticationRepository: authenticationRepository,
-          tokensRepository: tokensRepository,
+          authenticationRepository: _authenticationRepository,
+          tokensRepository: _tokensRepository,
         )..add(AuthenticationStatusChanged(AuthenticationStatus.unknown)),
         child: AuthenticationView(),
       ),
--- lib/authentication/view/authentication_view.dart
+++ lib/authentication/view/authentication_view.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:iurc_mobile_app/authentication/bloc/authentication_bloc.dart';
-import 'package:iurc_mobile_app/authentication/repositories/authentication_repository.dart';
+import 'package:iurc_mobile_app/authentication/authentication.dart';
+
+import 'package:iurc_mobile_app/login/login.dart';
 import 'package:iurc_mobile_app/screens/screens.dart';
 
 class AuthenticationView extends StatelessWidget {
@@ -9,52 +10,17 @@ class AuthenticationView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocListener<AuthenticationBloc, AuthenticationState>(
-      listener: (context, state) {
+    return BlocBuilder<AuthenticationBloc, AuthenticationState>(
+      builder: (context, state) {
         switch (state.status) {
+          case AuthenticationStatus.unknown:
+            return LoadingScreen();
           case AuthenticationStatus.authenticated:
-            // Navigator.of(context).pushNamedAndRemoveUntil("/home", (route) => false);
-            Navigator.of(context).pushAndRemoveUntil(
-              MaterialPageRoute(
-                builder: (context) => _ScreenStub(title: "Main Page"),
-              ),
-              (route) => false,
-            );
-            break;
+            return HomeScreen();
           case AuthenticationStatus.unauthenticated:
-            // Navigator.of(context).pushNamedAndRemoveUntil("/login", (route) => false);
-            Navigator.of(context).pushAndRemoveUntil(
-              MaterialPageRoute(
-                builder: (context) => _ScreenStub(title: "Sign In"),
-              ),
-              (route) => false,
-            );
-            break;
-          default:
+            return LoginScreen();
         }
       },
-      child: LoadingScreen(),
-    );
-  }
-}
-
-class _ScreenStub extends StatelessWidget {
-  final String title;
-  const _ScreenStub({
-    Key? key,
-    required this.title,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text(title)),
-      body: Center(
-        child: Text(
-          title,
-          style: Theme.of(context).textTheme.displayLarge,
-        ),
-      ),
     );
   }
 }
