0f8b81608b9260d536a33c7c4d0b1124dfdcc383
Nerower <p0sirega121234@gmail.com>
2022-04-09 01:42:51 +0300

    repos: Create events_repository

---
+++ lib/repositories/events_repository.dart
@@ -0,0 +1,18 @@
+import 'package:dio/dio.dart';
+import 'package:iurc_mobile_app/api/api.dart';
+import 'package:iurc_mobile_app/models/models.dart';
+
+class EventsRepository {
+  Api _api = Api();
+
+  Future<List<Event>> fetchEvents({required DateTime date}) async {
+    Response response = await _api.dio.get(
+      "/news",
+      queryParameters: {"date": "${date.year}-${date.month}-${date.day}"},
+    );
+
+    final data = response.data as List;
+
+    return data.map((json) => Event.fromJson(json)).toList();
+  }
+}
--- lib/repositories/repositories.dart
+++ lib/repositories/repositories.dart
@@ -1,2 +1,3 @@
+export 'events_repository.dart';
 export 'posts_repository.dart';
 export 'tokens_repository.dart';
